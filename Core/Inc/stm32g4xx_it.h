05a880fdef1e5e7dd7e1a2e930aad074b1ae3c52
dales-ghub <dale0001us@yahoo.com>
2025-03-09 11:01:29 -0600

    Update stm32g4xx_it.h

--- Core/Inc/stm32g4xx_it.h
+++ Core/Inc/stm32g4xx_it.h
@@ -21,31 +21,8 @@
 #ifndef __STM32G4xx_IT_H
 #define __STM32G4xx_IT_H
 
-#ifdef __cplusplus
- extern "C" {
-#endif
+extern uint8_t SPI2_DMAx;
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Exported types ------------------------------------------------------------*/
-/* USER CODE BEGIN ET */
-
-/* USER CODE END ET */
-
-/* Exported constants --------------------------------------------------------*/
-/* USER CODE BEGIN EC */
-
-/* USER CODE END EC */
-
-/* Exported macro ------------------------------------------------------------*/
-/* USER CODE BEGIN EM */
-
-/* USER CODE END EM */
-
-/* Exported functions prototypes ---------------------------------------------*/
 void NMI_Handler(void);
 void HardFault_Handler(void);
 void MemManage_Handler(void);
@@ -57,13 +34,5 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void USB_LP_IRQHandler(void);
 void TIM4_IRQHandler(void);
-
-/* USER CODE BEGIN EFP */
-
-/* USER CODE END EFP */
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* __STM32G4xx_IT_H */
