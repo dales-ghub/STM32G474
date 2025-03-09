2173896d95264f2a0c865852af1e36c2e45faef1
dales-ghub <dale0001us@yahoo.com>
2025-03-09 10:59:08 -0600

    Update adc.h

--- Core/Inc/adc.h
+++ Core/Inc/adc.h
@@ -9,3 +9,18 @@ void ADC3_Init(void);
 void ADC4_Init(void);
 void ADC5_Init(void);
 
+    /* 
+    Internal temperature sensor, temperature at which temperature sensor
+    has been calibrated in production for data into TEMPSENSOR_CAL2_ADDR
+    (tolerance: +-5 DegC) (unit: DegC). 
+    */
+#define TEMPSENSOR_CAL1_ADDR               ((uint16_t*) (0x1FFF75A8UL))
+#define TEMPSENSOR_CAL1_TEMP               (30L)
+#define TEMPSENSOR_CAL2_TEMP               (130L)
+#define TEMPSENSOR_CAL2_ADDR               ((uint16_t*) (0x1FFF75CAUL))
+#define TEMPSENSOR_CAL_VREFANALOG          (3000UL)
+/*
+temp = (TEMPSENSOR_CAL2_TEMP - TEMPSENSOR_CAL1_TEMP) /
+       (*TEMPSENSOR_CAL2_ADDR - *TEMPSENSOR_CAL1_ADDR) *
+       (TS_DATA - TEMPSENSOR_CAL1_ADDR) + 
+*/
